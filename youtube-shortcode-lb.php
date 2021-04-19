b685169c8d2f36e85e3f83925e306f152e9b4575
Lucas Bonomo <bonomo.lucas@gmail.com>
2021-04-19 08:18:24 -0300

    WordPress  coding standards

--- youtube-shortcode-lb.php
+++ youtube-shortcode-lb.php
@@ -17,38 +17,53 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// Requirements:
-// The following requirements must be met to consider this code challenge completed.
-//  - The shortcode must be able to receive a YouTube Video ID (f1nxZBAMpUs) as an attribute. If this attribute is present, the shortcode will display a YouTube Video Player in the post.
-//  - The shortcode must be able to receive a Video URL (https://www.youtube.com/watch?v=f1nxZBAMpUs) as one of its attributes. When this attribute is present, the shortcode will display an HTML5 Video Player in the post.
-//  - In both scenarios, the passed attribute is used as source for the Video Players.
+/**
+ * Requirements:
+ * The following requirements must be met to consider this code challenge completed.
+ * - The shortcode must be able to receive a YouTube Video ID (f1nxZBAMpUs) as an attribute. If this attribute is present, the shortcode will display a YouTube Video Player in the post.
+ * - The shortcode must be able to receive a Video URL (https://www.youtube.com/watch?v=f1nxZBAMpUs) as one of its attributes. When this attribute is present, the shortcode will display an HTML5 Video Player in the post.
+ * - In both scenarios, the passed attribute is used as source for the Video Players.
+ **/
 
-function yslb_render_ID($id) {
-	// Validate ID (string)
-	if ( 11 === strlen($id) ) {
+/**
+ * Render with video ID.
+ *
+ * @param string $id Youtube video ID.
+ */
+function yslb_render_id( $id ) {
+	// Validate ID (string).
+	if ( 11 === strlen( $id ) ) {
 		return '<div class="yslb-container"> '
-		. sprintf('<iframe class="yslb-iframe" src="https://www.youtube.com/embed/%s"></iframe>', $id)
+		. sprintf( '<iframe class="yslb-iframe" src="https://www.youtube.com/embed/%s"></iframe>', $id )
 		. '</div>';
 	} else {
-		if ( WP_DEBUG ) { error_log(__( "Youtube video ID incorrect size", "youtube-shortcode-lb" )); }
+		if ( WP_DEBUG ) { error_log( __( 'Youtube video ID incorrect size', 'youtube-shortcode-lb' ) ); }// phpcs:ignore
 		return yslb_render_help();
 	}
 }
 
-function yslb_render_URL($url) {
+/**
+ * Render with video URL.
+ *
+ * @param string $url Youtube video URL.
+ */
+function yslb_render_url( $url ) {
 	// Validate URL.
 	if ( wp_http_validate_url( $url ) ) {
-		return '<div class="yslb-container">' . wp_video_shortcode( array('src' => $url ) ) . '</div>';
+		return '<div class="yslb-container">' . wp_video_shortcode( array( 'src' => $url ) ) . '</div>';
 	} else {
-		if ( WP_DEBUG ) { error_log(__( "URL parameter is incorrect", "youtube-shortcode-lb" )); }
+		if ( WP_DEBUG ) { error_log( __( 'URL parameter is incorrect', 'youtube-shortcode-lb' ) ); } // phpcs:ignore
 		return yslb_render_help();
 	}
 }
 
+/**
+ * Render help.
+ */
 function yslb_render_help() {
 	return '<div class="yslb-container-error">
 <h3>Error</h3>
-	<p> ' . __( "For example:", "youtube-shortcode-lb" ) . '
+	<p> ' . __( 'For example:', 'youtube-shortcode-lb' ) . '
 		<ul>
 			<li>[youtube-shortcode id="f1nxZBAMpUs"]</li>
 			<li>[youtube-shortcode url="https://www.youtube.com/watch?v=f1nxZBAMpUs"]</li>
@@ -57,47 +72,52 @@ function yslb_render_help() {
 </div>';
 }
 
-// function yslb_register_shortcode( $atts ) {
+/**
+ * Main proces.
+ *
+ * @param array $atts Shortcode parameters.
+ */
 function yslb_shortcode( $atts ) {
 
 	// Checked the number of parameters.
-	if ( is_array($atts) ) {
-		if (1 !== sizeof($atts)) {
-			if ( WP_DEBUG ) { error_log(__( "Incorrect number of parameters", "youtube-shortcode-lb" )); }
+	if ( is_array( $atts ) ) {
+		if ( 1 !== count( $atts ) ) {
+			if ( WP_DEBUG ) { error_log( __( 'Incorrect number of parameters', 'youtube-shortcode-lb' ) ); } // phpcs:ignore
 			return yslb_render_help();
 		} else {
-			$valid_attributes = array('url', 'id');
-			if ( ! ( array_key_exists('url', $atts) ||  array_key_exists('id', $atts) ) ) {
-				if ( WP_DEBUG ) { error_log(__( "Incorrect name of parameter", "youtube-shortcode-lb" )); }
+			$valid_attributes = array( 'url', 'id' );
+			if ( ! ( array_key_exists( 'url', $atts ) || array_key_exists( 'id', $atts ) ) ) {
+				if ( WP_DEBUG ) { error_log( __( 'Incorrect name of parameter', 'youtube-shortcode-lb' ) ); } // phpcs:ignore
 				return yslb_render_help();
 			}
 		}
 	} else {
-		if ( WP_DEBUG ) { error_log(__( "Without parameter", "youtube-shortcode-lb" )); }
+		if ( WP_DEBUG ) { error_log( __( 'Without parameter', 'youtube-shortcode-lb' ) ); } // phpcs:ignore
 		return yslb_render_help();
 	}
 
 	// Enqueue style.
 	wp_enqueue_style( 'youtube-shortcode-lb', plugin_dir_url( __FILE__ ) . '/youtube-shortcode-lb.css', array(), '1.0.0', 'all' );
-	
-	switch (array_keys($atts)[0]) {
+
+	switch ( array_keys( $atts )[0] ) {
 		case 'url':
-			return yslb_render_URL($atts['url']);
+			return yslb_render_url( $atts['url'] );
 		case 'id':
-			return yslb_render_ID($atts['id']);
+			return yslb_render_id( $atts['id'] );
 	}
 }
 
-add_shortcode( 'youtube-shortcode', 'yslb_shortcode' );
-add_action( 'init', 'yslb_load_textdomain' );
-  
 /**
  * Load plugin textdomain.
  */
 function yslb_load_textdomain() {
-	load_plugin_textdomain( 
-		'youtube-shortcode-lb', 
+	load_plugin_textdomain(
+		'youtube-shortcode-lb',
 		false,
-		 dirname( plugin_basename( __FILE__ ) ) . '/languages' 
+		dirname( plugin_basename( __FILE__ ) ) . '/languages'
 	);
+
 }
+
+add_shortcode( 'youtube-shortcode', 'yslb_shortcode' );
+add_action( 'init', 'yslb_load_textdomain' );
